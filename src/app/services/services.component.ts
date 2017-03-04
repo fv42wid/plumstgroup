ef2ded6f3522497982f122cf40005214c3a83fad
Frank Vinci <fv42wid@yahoo.com>
2017-03-04 15:58:52 -0500

    select service tab from url

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-services',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./services.component.css']
 })
 export class ServicesComponent implements OnInit {
+  service: number = 1;
 
-  constructor() { }
+  constructor(
+    public route: ActivatedRoute
+  ) {
+    console.log('construct');
+    console.log(this.route.params['_value']['id']);
+  }
 
   ngOnInit() {
+    this.service = this.route.params['_value']['id'];
+    console.log('init');
+    console.log(this.route.params);
   }
 
 }
